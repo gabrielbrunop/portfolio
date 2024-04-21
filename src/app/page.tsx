fa66cb145a3834c9b3bf30a53ba43b21abc72a7c
gabrielbrunop <gabrielbrunoes@gmail.com>
2024-04-21 18:33:19 -0300

    make carousel full width in mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
       <div className="flex flex-row px-[12%] sm:px-[20%] py-12">
         <Navigation />
       </div>
-      <main className="flex flex-col gap-16 px-[8%] sm:px-[20%] py-12 antialiased pb-32">
-        <section className="flex flex-col gap-8">
+      <main className="flex flex-col gap-16 py-12 antialiased pb-32">
+        <section className="flex flex-col px-[8%] sm:px-[20%] gap-8">
           <p className="font-medium text-xl sm:text-2xl text-indigo-400">Olá, meu nome é</p>
           <div className="flex flex-col gap-2">
             <h1 className="text-5xl sm:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-slate-400 to-slate-500 font-sans font-bold">
@@ -35,30 +35,34 @@ export default function Home() {
         <section className="flex flex-col gap-24">
           <SectionScroll sectionName="tecnologias" />
           <div className="flex flex-col gap-12">
-            <h2 className="font-bold text-2xl sm:text-3xl text-slate-500">Eu adoro aprender novas linguagens e frameworks!</h2>
-            <div className="flex flex-col items-center justify-center">
+            <div className="px-[8%] sm:px-[20%]">
+              <h2 className="font-bold text-2xl sm:text-3xl text-slate-500">Eu adoro aprender novas linguagens e frameworks!</h2>
+            </div>
+            <div className="flex flex-col sm:px-[20%] items-center justify-center">
               <TechCarousel />
             </div>
           </div>
-          <Alert className="bg-slate-900 border-slate-900">
-            <AlertTitle className="flex flex-row gap-2 text-lg text-slate-300">
-              <HiMiniRocketLaunch color="#cccccc" className="text-4xl h-8 w-6" />
-              <span>Muitas tecnologias</span>
-            </AlertTitle>
-            <AlertDescription>
-              <p className="sm:text-lg text-slate-400 pl-4 sm:pl-8">
-                Atualmente eu tenho {getAge(new Date("2002-07-11"))} anos, mas programo como hobby desde os 13. Aprendi muitas, muitas linguagens ao longo dos anos - aqui
-                só menciono as mais recentes ou que tenho maior conhecimento, mas é muita coisa, mesmo!
-              </p>
-            </AlertDescription>
-          </Alert>
+          <div className="px-[8%] sm:px-[20%]">
+            <Alert className="bg-slate-900 border-slate-900">
+              <AlertTitle className="flex flex-row gap-2 text-lg text-slate-300">
+                <HiMiniRocketLaunch color="#cccccc" className="text-4xl h-8 w-6" />
+                <span>Muitas tecnologias</span>
+              </AlertTitle>
+              <AlertDescription>
+                <p className="sm:text-lg text-slate-400 pl-4 sm:pl-8">
+                  Atualmente eu tenho {getAge(new Date("2002-07-11"))} anos, mas programo como hobby desde os 13. Aprendi muitas, muitas linguagens ao longo dos anos - aqui
+                  só menciono as mais recentes ou que tenho maior conhecimento, mas é muita coisa, mesmo!
+                </p>
+              </AlertDescription>
+            </Alert>
+          </div>
         </section>
-        <section className="flex flex-col gap-12">
+        <section className="flex flex-col px-[8%] sm:px-[20%] gap-12">
           <SectionScroll sectionName="projetos" />
           <h2 className="font-bold text-2xl sm:text-3xl text-slate-500">Meus projetos</h2>
           <MyProjects />
         </section>
-        <section className="flex flex-col gap-12">
+        <section className="flex flex-col px-[8%] sm:px-[20%] gap-12">
           <SectionScroll sectionName="contato" />
           <h2 className="font-bold text-2xl sm:text-3xl text-slate-500">Contato</h2>
           <ContactList />
